43b17958689f7d390177d370d9155771fc61b03c
minifull <kxwangzju@gmail.com>
2023-11-08 18:07:53 +0800

    add result

---
+++ dataAugment.py
@@ -0,0 +1,8 @@
+# Augemnting data in traffic dataset
+import cv2
+import numpy as np
+import os
+import random
+
+# random flip
+
--- resnet-cifar100.py
+++ resnet-cifar100.py
@@ -1,4 +1,5 @@
 import time
+from typing import Callable, List, Optional, Type, Union
 import configargparse
 import torch
 import torch.nn as nn
@@ -18,10 +19,113 @@ from ignite.handlers import *
 results = []
 losses = []
 
+class BottleNeck(nn.Module):
+    """Residual block for resnet over 50 layers
+    """
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BottleNeck, self).__init__()
+        self.residual_branch = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels,
+                      out_channels,
+                      stride=stride,
+                      kernel_size=3,
+                      padding=1,
+                      bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels,
+                      out_channels * BottleNeck.expansion,
+                      kernel_size=1,
+                      bias=False),
+            nn.BatchNorm2d(out_channels * BottleNeck.expansion),
+        )
+
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_channels != out_channels * BottleNeck.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels,
+                          out_channels * BottleNeck.expansion,
+                          stride=stride,
+                          kernel_size=1,
+                          bias=False),
+                nn.BatchNorm2d(out_channels * BottleNeck.expansion))
+
+    def forward(self, x):
+        return nn.ReLU(inplace=True)(self.residual_branch(x) +
+                                     self.shortcut(x))
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=100, inter_layer=False):
+        super(ResNet, self).__init__()
+        self.inter_layer = inter_layer
+        self.in_channels = 64
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64), nn.ReLU(inplace=True))
+
+        self.stage2 = self._make_layer(block, 64, layers[0], 1)
+        self.stage3 = self._make_layer(block, 128, layers[1], 2)
+        self.stage4 = self._make_layer(block, 256, layers[2], 2)
+        self.stage5 = self._make_layer(block, 512, layers[3], 2)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        """make resnet layers(by layer i didnt mean this 'layer' was the 
+        same as a neuron netowork layer, ex. conv layer), one layer may 
+        contain more than one residual block 
+        Args:
+            block: block type, basic block or bottle neck block
+            out_channels: output depth channel number of this layer
+            num_blocks: how many blocks per layer
+            stride: the stride of the first block of this layer
+        
+        Return:
+            return a resnet layer
+        """
+
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+
+        if self.inter_layer:
+            x1 = self.stage2(x)
+            x2 = self.stage3(x1)
+            x3 = self.stage4(x2)
+            x4 = self.stage5(x3)
+            x = self.avg_pool(x4)
+            x = x.view(x.size(0), -1)
+            x = self.fc(x)
+
+            return [x1, x2, x3, x4, x]
+        else:
+            x = self.stage2(x)
+            x = self.stage3(x)
+            x = self.stage4(x)
+            x = self.stage5(x)
+            x = self.avg_pool(x)
+            x = x.view(x.size(0), -1)
+            x = self.fc(x)
+
+            return x
 
 class ResNetCustom(ResNet):
 
-    def __init__(self, block, layers, num_classes=1000):
+    def __init__(self, block, layers, num_classes=100):
         super(ResNet, self).__init__()
         self._norm_layer = nn.BatchNorm2d
 
@@ -49,22 +153,6 @@ class ResNetCustom(ResNet):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.shape[0], -1)
-        x = self.fc(x)
-
-        return x
-
 def resnet10(**kwargs):
     return ResNetCustom(BasicBlock, [1, 1, 1, 1], **kwargs)
 
@@ -72,7 +160,7 @@ def resnet18(**kwargs):
     return ResNetCustom(BasicBlock, [2, 2, 2, 2], **kwargs)
 
 def resnet50(**kwargs):
-    return ResNetCustom(Bottleneck, [3, 4, 6, 3], **kwargs)
+    return ResNet(BottleNeck, [3, 4, 6, 3], **kwargs)
 
 
 def logger(engine, model, evaluator, loader, pbar):
@@ -146,11 +234,11 @@ if __name__ == '__main__':
     # model = torchvision.models.resnet50(num_classes=100, pretrained=True)
 
     # read model from file
-    model.load_state_dict(torch.load('./load/model.pth'))
-    modules = model.modules()
-    for p in modules:
-        if p._get_name()!= 'Linear':
-            p.requires_grad = False
+    # model.load_state_dict(torch.load('./load/model.pth'))
+    # modules = model.modules()
+    # for p in modules:
+    #     if p._get_name()!= 'Linear':
+    #         p.requires_grad = False
     model.to(device)
             
 
