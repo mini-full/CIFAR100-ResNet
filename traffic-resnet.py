25385f05c8bedcf403c3ba2b7a5999f0e903fc7f
minifull <kxwangzju@gmail.com>
2023-11-08 21:43:05 +0800

    add autoaugment

--- traffic-resnet.py
+++ traffic-resnet.py
@@ -64,9 +64,10 @@ if __name__ == '__main__':
     # data augmentation by AutoAugmentPolicy
     train_transform = transforms.Compose([
         transforms.Resize((224, 224)),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(10),
-        transforms.RandomAffine(0, shear=10, scale=(0.8, 1.2)),
+        # transforms.RandomHorizontalFlip(),
+        # transforms.RandomRotation(10),
+        # transforms.RandomAffine(0, shear=10, scale=(0.8, 1.2)),
+        transforms.AutoAugment(),
         transforms.ToTensor(),
         # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
         #                      0.229, 0.224, 0.225])
